17e4cf76299b96774b986ff042308761271bf1a1
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-04 16:27:59 +0100

    Customers ViewModel

---
+++ ViewModels/CustomersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieApp.Models;
+
+namespace MovieApp.ViewModels
+{
+    public class CustomersViewModel
+    {
+        public List<Customer> Customers { get; set; }
+    }
+}
