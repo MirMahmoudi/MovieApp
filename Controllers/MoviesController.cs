52cd222714325c4fc9de3acca362eb522727d23c
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-04 17:00:49 +0100

    create Index page for Movie part

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MovieApp.Models;
+using MovieApp.ViewModels;
 
 namespace MovieApp.Controllers
 {
@@ -11,7 +13,23 @@ namespace MovieApp.Controllers
         // GET: Movies
         public ActionResult Index()
         {
-            return View();
+            var movie1 = new Movie
+            {
+                Name = "Die Hard"
+            };
+            
+            var movie2 = new Movie
+            {
+                Name = "Forest Gump"
+            };
+
+            var moviesList = new List<Movie>();
+            moviesList.Add(movie1);
+            moviesList.Add(movie2);
+
+            var movies = new MoviesViewModel { Movies = moviesList };
+
+            return View(movies);
         }
     }
 }
---
+++ ViewModels/MoviesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieApp.Models;
+
+namespace MovieApp.ViewModels
+{
+    public class MoviesViewModel
+    {
+        public List<Movie> Movies { get; set; }
+    }
+}
