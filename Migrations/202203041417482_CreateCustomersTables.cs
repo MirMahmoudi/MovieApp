1358196d750d9b853f7dd3523404c54b0c5fccde
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-04 15:22:02 +0100

    Add Customers Table to DataBase

---
+++ Migrations/202203041417482_CreateCustomersTables.cs
@@ -0,0 +1,28 @@
+﻿namespace MovieApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateCustomersTables : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        Birthdate = c.DateTime(nullable: false, precision: 7, storeType: "datetime2"),
+                        IsSubscribedNewsLetter = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
