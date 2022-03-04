7f1b479fad7b19f3b91179961eddb95025b6c580
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-04 15:36:01 +0100

    MembershipType Model

---
+++ Models/MembershipType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp.Models
+{
+    public class MembershipType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public float SignUpFee { get; set; }
+
+        public int DurationInMonths { get; set; }
+
+        public int DiscountRate { get; set; }
+    }
+}
