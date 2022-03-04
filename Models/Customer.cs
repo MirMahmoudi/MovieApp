e5b16e570394dffd229ccbb44e39203447fdc8da
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-04 15:11:45 +0100

    Add "Customer" & "Movie" Models to App

---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime Birthdate { get; set; }
+
+        public bool IsSubscribedNewsLetter { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime DateAdded { get; set; }
+
+        public int NumberInStock { get; set; }
+
+        public int NumberAvailable { get; set; }
+    }
+}
