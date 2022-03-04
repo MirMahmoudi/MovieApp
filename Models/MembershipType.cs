4f23f8a7258074d4ef0180f9beeb4278f050cdbe
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-04 15:39:06 +0100

    Update MembershipType MOdel

--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace MovieApp.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public float SignUpFee { get; set; }
