0c99088a0f6e5f7e44c7fa759897949fb7cb1efd
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-04 15:39:35 +0100

    Add MembershipType to Customer Model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using MovieApp.Models;
 
 namespace MovieApp.Models
 {
@@ -19,5 +20,9 @@ namespace MovieApp.Models
         public DateTime Birthdate { get; set; }
 
         public bool IsSubscribedNewsLetter { get; set; }
+
+        public int MembershipTypeId { get; set; }
+
+        public MembershipType MembershipType { get; set; }
     }
 }
