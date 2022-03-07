a30ae9a19095029334170f3838227c4ad12197f4
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-07 10:28:08 +0100

    add custom validation to customers: SubscribedToNewsLetter

---
+++ Models/SubscribedToNewsLetter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using MovieApp.ViewModels;
+
+namespace MovieApp.Models
+{
+    public class SubscribedToNewsLetter : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (CustomerFormViewModel)validationContext.ObjectInstance;
+
+            if (customer.IsSubscribedNewsLetter)
+                return ValidationResult.Success;
+
+            return new ValidationResult("Ypu should subscribe the newsletter!");
+        }
+    }
+}
