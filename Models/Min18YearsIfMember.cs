d25cae5c0a1f10081b3e4e4792cfc75830606b66
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-07 10:27:10 +0100

    add custom validation to customers: "Min18YearsIfMember"

---
+++ Models/Min18YearsIfMember.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using MovieApp.Models;
+using MovieApp.ViewModels;
+
+namespace MovieApp.Models
+{
+    public class Min18YearsIfMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (CustomerFormViewModel)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == CustomerFormViewModel.MembershipUnknown ||
+                customer.MembershipTypeId == CustomerFormViewModel.MembershipPayAsYouGo)
+                return ValidationResult.Success;
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthdate is required!");
+
+            var age = DateTime.Now.Year - customer.Birthdate.Value.Year;
+
+            return (age >= 18)
+                   ? ValidationResult.Success
+                   : new ValidationResult("Customer should be at least 18 years old to go on a membership!");
+
+        }
+    }
+}
