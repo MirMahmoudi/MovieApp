d41cad8846c3eaf6fe7772b824bfd1bc69f58ae4
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-07 10:22:25 +0100

    Add-Migration: "MakeBirthdateNullableInCustomersTable"

---
+++ Migrations/202203061100311_MakeBirthdateNullableInCustomersTable.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MakeBirthdateNullableInCustomersTable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Birthdate", c => c.DateTime(precision: 7, storeType: "datetime2"));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Birthdate", c => c.DateTime(nullable: false, precision: 7, storeType: "datetime2"));
+        }
+    }
+}
