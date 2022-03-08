052f80003acb093c9b4834c8482e115cd194b1b4
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-08 15:42:13 +0100

    AutoMapper

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using MovieApp.Dtos;
+using MovieApp.Models;
+
+namespace MovieApp.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain To Dto
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+            Mapper.CreateMap<Genre, GenreDto>();
+
+            // Dto To Domain
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+        }
+    }
+}
