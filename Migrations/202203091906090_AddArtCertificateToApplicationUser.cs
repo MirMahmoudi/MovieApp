2ad930db39a8ddddc90ed918a87f12bfeb4c3a93
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-03-09 20:17:45 +0100

    Add-Migration: "AddArtCertificateToApplicationUser"

---
+++ Migrations/202203091906090_AddArtCertificateToApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddArtCertificateToApplicationUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "ArtCertificate", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "ArtCertificate");
+        }
+    }
+}
